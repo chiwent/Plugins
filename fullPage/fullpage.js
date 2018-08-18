13209b77c3f7f9b50903c505d7c1773c10c8fd59
chiwent <atsimonchen@163.com>
2018-08-18 10:38:08 +0800

    增加导航条和导航条切换功能

--- fullPage/fullpage.js
+++ fullPage/fullpage.js
@@ -1,9 +1,13 @@
+//TODO:
+
+
+
 class FullPage {
     constructor(options) {
             const defaultOptions = {
                 wrapper: '',
                 pagesEle: [],
-                pagesNum: '',
+                dots: [],
                 delay: 1000,
                 isShowDots: true,
                 callback: () => {}
@@ -12,10 +16,10 @@ class FullPage {
             this.callback = this.options.callback;
             this.wrapper = this.options.wrapper;
             this.pagesEle = this.options.pagesEle;
-            this.pagesNum = this.options.pagesNum;
-            this.dots = [];
+            this.pagesNum = this.pagesEle.length;
+            this.dots = this.options.dots;
             this.pageHeight = document.documentElement.clientHeight || document.body.clientHeight;
-            this.currentIndex = 0;
+            this.currentIndex = 1;
             this.delay = this.options.delay;
             this.startX = undefined;
             this.startY = undefined;
@@ -26,6 +30,10 @@ class FullPage {
             this.now = 0;
             this.sliderTimer = null;
             this.endTime = 0;
+            this.index = 1;
+            this.isShowDots = this.options.isShowDots;
+            this.currection = 1;
+            this.backup = 1;
         }
         /*
         extends() {
@@ -47,26 +55,31 @@ class FullPage {
         for (var i = 0; i < this.pagesNum; i++) {
             this.pagesEle[i].style.height = this.pageHeight + "px";
         }
-        //var _this = this;
         this.initMousewheel();
         this.initTouch();
+        if (this.isShowDots) {
+            this.clickHandler();
+        } else {
+            document.getElementsByClassName('pagenavigation')[0].style.display = 'none';
+        }
     }
+
     initMousewheel() {
         if ((navigator.userAgent.toLowerCase().indexOf("firefox") != -1)) {
-            document.addEventListener("DOMMouseScroll", this.scrollFn.bind(this), false);
+            document.addEventListener("DOMMouseScroll", this.throttle(this.scrollFn).bind(this), false);
         } else if (document.addEventListener) {
-            document.addEventListener("mousewheel", this.scrollFn.bind(this), false);
+            document.addEventListener("mousewheel", this.throttle(this.scrollFn).bind(this), false);
         } else if (document.attachEvent) {
-            document.attachEvent("onmousewheel", this.scrollFn.bind(this));
+            document.attachEvent("onmousewheel", this.throttle(this.scrollFn).bind(this));
         } else {
-            document.onmousewheel = this.scrollFn.bind(this);
+            document.onmousewheel = this.throttle(this.scrollFn).bind(this);
         }
     }
     initTouch() {
-        this.addEventHandler(document, 'touchstart', this.touchStart.bind(this));
-        this.addEventHandler(document, 'touchend', this.touchEnd.bind(this));
-        this.addEventHandler(document, 'touchmove', this.touchMove.bind(this));
-        this.addEventHandler(document, 'touchcancel', this.touchCancel.bind(this));
+        this.addEventHandler(document, 'touchstart', this.throttle(this.touchStart).bind(this));
+        this.addEventHandler(document, 'touchend', this.throttle(this.touchEnd).bind(this));
+        this.addEventHandler(document, 'touchmove', this.throttle(this.touchMove).bind(this));
+        this.addEventHandler(document, 'touchcancel', this.throttle(this.touchCancel).bind(this));
     }
     addEventHandler(oTarget, sEventType, fnHandler) {
         if (oTarget.addEventListener) {
@@ -76,33 +89,42 @@ class FullPage {
         } else {
             oTarget["on" + sEventType] = fnHandler;
         }
-    };
-    scrollFn(e) {
-        var startTime = new Date().getTime();
-        //var endTime = 0;
-        var delta = e.detail || (-e.wheelDelta);
-        if ((this.endTime - startTime) < -this.delay) {
-            if (delta > 0 && parseInt(this.wrapper.offsetTop) > -(this.pageHeight * (this.pagesNum - 1))) {
-                this.now = this.now - this.pageHeight;
-                this.turnToPage(this.now, "down").then(this.callback);
-                this.callback();
-            }
-            if (delta < 0 && parseInt(this.wrapper.offsetTop) < 0) {
-                this.now = this.now + this.pageHeight;
-                this.turnToPage(this.now, "up").then(this.callback);
-                this.callback();
-            }
-            this.endTime = new Date().getTime();
-        } else {
-            e.preventDefault();
-        }
     }
-
+    toggleActive() {
+        for (let item of this.dots) {
+            this.removeClass(item, 'active');
+        }
+        this.addClass(this.dots[this.currentIndex - 1], 'active');
+    }
+    scrollFn(e) {
+        // var startTime = new Date().getTime();
+        // var endTime = 0;
+        var delta = e.detail || (-e.wheelDelta);
+        //if ((this.endTime - startTime) < -this.delay) {
+        if (delta > 0 && parseInt(this.wrapper.offsetTop) > -(this.pageHeight * (this.pagesNum - 1))) {
+            this.now = this.now - this.pageHeight;
+            this.currentIndex++;
+            this.toggleActive();
+            this.turnToPage(this.now, "down").then(this.callback);
+            // this.callback();
+        }
+        if (delta < 0 && parseInt(this.wrapper.offsetTop) < 0) {
+            this.now = this.now + this.pageHeight;
+            this.currentIndex--;
+            this.toggleActive();
+            this.turnToPage(this.now, "up").then(this.callback);
+            // this.callback();
+        }
+        this.endTime = new Date().getTime();
+        // } else {
+        //     e.preventDefault();
+        // }
+    }
     turnToPage(now, direction) {
-        // var _this = this;
+        var _this = this;
         return new Promise((resolve, reject) => {
             if (resolve) {
-                this.move(now, direction);
+                this.move(this.now, direction);
                 resolve();
             } else {
 
@@ -117,12 +139,13 @@ class FullPage {
         this.sliderTimer = setInterval(() => {
             let speed = 0;
             if (direction === "down") {
-                if (this.now < 0 && this.now < this.wrapper.offsetTop) {
+
+                if (this.now < 0 && this.now < this.wrapper.offsetTop && this.currentIndex < this.pagesNum) {
+                    // this.currentIndex = this.currentIndex < this.pagesNum ? this.currentIndex += 1 : 0;
                     speed = -10;
                     this.wrapper.style.top = this.wrapper.offsetTop + speed + "px";
                     if (this.wrapper.style.top <= this.now) {
                         this.wrapper.style.top = this.now + "px";
-                        console.log('2:', this.wrapper.style.top);
                     }
                 } else {
                     this.wrapper.style.top = this.now + "px";
@@ -130,7 +153,8 @@ class FullPage {
                     clearInterval(this.sliderTimer);
                 }
             } else if (direction === "up") {
-                if (this.now <= 0 && this.now >= this.wrapper.offsetTop) {
+                if (this.now <= 0 && this.now >= this.wrapper.offsetTop && this.currentIndex > 1) {
+                    // this.currentIndex = this.currentIndex >= 0 ? this.currentIndex - 1 : this.pagesNum;
                     speed = 10;
                     this.wrapper.style.top = this.wrapper.offsetTop + speed + "px";
                     if (this.wrapper.style.top >= this.now) {
@@ -153,33 +177,35 @@ class FullPage {
     }
     slide(e) {
         e = e || window.event;
-        var startTime = new Date().getTime();
-        //var endTime = 0;
-        if ((this.endTime - startTime) < -this.delay) {
-            if (this.endY < this.startY && parseInt(this.wrapper.offsetTop) > -(this.pageHeight * (this.pagesNum - 1))) {
-                this.now = this.now - this.pageHeight;
-                this.turnToPage(this.now, "down").then(this.callback);
-                this.callback();
-            }
-            if (this.endY > this.startY && parseInt(this.wrapper.offsetTop) < 0) {
-                this.now = this.now + this.pageHeight;
-                this.turnToPage(this.now, "up").then(this.callback);
-                this.callback();
-            }
-            this.endTime = new Date().getTime();
-        } else {
-            e.preventDefault();
+        // var startTime = new Date().getTime();
+        // var endTime = 0;
+        // console.log('slide current ', this.currentIndex)
+        //if ((this.endTime - startTime) < -this.delay) {
+        // console.log(this.currentIndex)
+        if (this.endY < this.startY && parseInt(this.wrapper.offsetTop) > -(this.pageHeight * (this.pagesNum - 1))) {
+            this.now = this.now - this.pageHeight;
+            this.currentIndex += 1 ? this.currentIndex < this.pagesNum : this.currentIndex = this.pagesNum;
+            console.log('down:', this.currentIndex);
+            this.toggleActive();
+            this.turnToPage(this.now, "down").then(this.callback);
+            // this.callback();
         }
-
-
-        // this.now = this.now - this.pageHeight;
-        // // console.log()
-        // console.log('this.now: ', this.now);
-        // if (this.endY > this.startY) {
-        //     this.turnToPage(this.now, 'up')
-        // } else if (this.endY < this.startY) {
-        //     this.turnToPage(this.now, 'down');
+        if (this.endY > this.startY && parseInt(this.wrapper.offsetTop) < 0) {
+            this.now = this.now + this.pageHeight;
+            this.currentIndex -= 1 ? this.currentIndex > 1 : this.currentIndex = 1;
+            console.log('up:', this.currentIndex);
+            this.toggleActive();
+            this.turnToPage(this.now, "up").then(this.callback);
+            // this.callback();
+        }
+        // if (this.wrapper.style.top === 0) {
+        //     this.currentIndex = 1;
         // }
+        //   this.endTime = new Date().getTime();
+        // } else {
+        //     e.preventDefault();
+        // }
+
     }
     touchStart(e) {
         e = e || widnow.event;
@@ -199,29 +225,113 @@ class FullPage {
         this.nowY = e.changedTouches[0].pageY;
     }
     touchCancel(e) {
-            e = e || window.event;
-            this.endX = e.changedTouches[0].pageX;
-            this.endY = e.changedTouches[0].pageY;
-            this.judgeSlide() && this.slide(e);
+        e = e || window.event;
+        this.endX = e.changedTouches[0].pageX;
+        this.endY = e.changedTouches[0].pageY;
+        this.judgeSlide() && this.slide(e);
+    }
+    hasClass(obj, className) {
+        return obj.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+    }
+    addClass(obj, className) {
+        if (!this.hasClass(obj, className))
+            obj.className += "" + className;
+    }
+    removeClass(obj, className) {
+        if (this.hasClass(obj, className)) {
+            var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+            obj.className = obj.className.replace(reg, '');
         }
-        /*
-        throttle(method, context, delay) {
-            let wait = false;
-            return function(...args) {
-                if (!wait) {
-                    method.apply(context, args);
-                    wait = true;
-                    setTimeout(() => {
-                        wait = false;
-                    }, delay);
-                }
-            };
+    }
+    toggleClass(obj, className) {
+        if (this.hasClass(obj, className)) {
+            this.removeClass(obj, className)
+        } else {
+            this.addClass(obj, className)
         }
-        debounce(method, context, event, delay) {
-            clearTimeout(method.tId);
-            method.tId = setTimeout(() => {
-                method.call(context, event);
-            }, delay);
+    }
+    judge() {
+
+        for (let item of this.dots) {
+            if (this.hasClass(item, 'active'))
+                return item.getAttribute('index')
         }
-        */
+    }
+    gotoPage(e, index) {
+        // console.log('Goto Page')
+        e = e || window.event;
+        //var startTime = new Date().getTime();
+        //var endTime = 0;
+        // if ((this.endTime - startTime) < -this.delay) {
+        // if (this.judge() !== index) {
+        if (this.backup < index && parseInt(this.wrapper.offsetTop) > -(this.pageHeight * (this.pagesNum - 1))) {
+            this.now = this.now - this.pageHeight * (Math.abs(index - this.backup));
+            // index += 1 ? index < this.pagesNum : index = this.pagesNum;
+            this.turnToPage(this.now, "down").then(() => {
+                console.log('down current2:', index)
+                this.removeClass(this.dots[this.backup - 1], 'active');
+                this.addClass(this.dots[index - 1], 'active');
+
+            }).then(this.callback);
+
+        }
+        if (this.backup > index) {
+            //this.toggleActive();
+            this.now = this.now + this.pageHeight * (Math.abs(index - this.backup));
+            this.turnToPage(this.now, "up").then(() => {
+                this.removeClass(this.dots[this.backup - 1], 'active');
+                this.addClass(this.dots[index - 1], 'active');
+
+            }).then(this.callback);
+        }
+        //this.endTime = new Date().getTime();
+        //this.index = index
+        //  } else {
+        //      e.preventDefault();
+        // }
+        // }
+    }
+
+    clickHandler(e) {
+
+        for (let item of this.dots) {
+
+            this.addEventHandler(item, 'click', (e) => {
+
+                this.backup = this.currentIndex
+                console.log('backup:', this.backup)
+                this.currentIndex = parseInt(item.getAttribute('index'));
+                // this.currection =
+                //     Math.abs(Math.ceil(this.wrapper.offsetTop / this.pageHeight))
+                this.gotoPage(e, this.currentIndex);
+            });
+        }
+
+    }
+
+    throttle(fn, interval = this.delay) {
+        let canRun = true;
+        return function() {
+            if (!canRun) return;
+            canRun = false;
+            setTimeout(() => {
+                fn.apply(this, arguments);
+                canRun = true;
+            }, interval);
+        };
+    }
+    debounce(fn, wait = this.delay) {
+        var timer = null;
+        return function() {
+            var context = this
+            var args = arguments
+            if (timer) {
+                clearTimeout(timer);
+                timer = null;
+            }
+            timer = setTimeout(function() {
+                fn.apply(context, args)
+            }, wait)
+        }
+    }
 }
